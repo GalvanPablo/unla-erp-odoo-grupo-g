b7e985dd85fcbf91d3e3827d2686050a9d804754
Pablo Galvan <pablogalvan.015@gmail.com>
2025-08-24 14:40:57 -0300

    1eros 10 puntos de laguia

---
+++ real_state/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Inmobiliaria",
+    "version": "1.0",
+    "author": "UNLa-Grupo-G",
+    "category": "UNLa",
+    "description": "Modulo creado para la catedra de Programación de Sistemas ERP con Odoo",
+    "depends": [
+		"base"
+	],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/real_estate_menuitem.xml"
+    ],
+    "application": True,
+    # "installable": True
+}
