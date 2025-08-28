409c12a4fd9ad646022d6a3abc9d02556bbdd60b
csantander93 <casdesarrollo.2022@gmail.com>
2025-08-28 00:12:45 -0300

    realizado hasta el punto 13 inclusive

---
+++ README.md
@@ -0,0 +1,1 @@
+# unla-grupo-g
---
+++ real_state/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Inmobiliaria",
+    "version": "1.0",
+    "author": "UNLa-Grupo-G",
+    "category": "UNLa",
+    "description": "Modulo creado para la catedra de Programación de Sistemas ERP con Odoo",
+    "depends": [
+		"base"
+	],
+    "data": [
+        'security/real_estate_res_groups.xml',
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/real_estate_menuitem.xml"
+    ],
+    "application": True,
+    # "installable": True
+}
